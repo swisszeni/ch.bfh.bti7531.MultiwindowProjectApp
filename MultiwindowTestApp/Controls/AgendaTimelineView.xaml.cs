e017359838d5c60879d0fbbce77e2d699789e877
Raphael Zenhäusern <raphael@zenhausern.net>
2016-05-10 19:28:48 +0200

    Initial Commit

---
+++ MultiwindowTestApp/Controls/AgendaTimelineView.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace MultiwindowTestApp.Controls
+{
+    public sealed partial class AgendaTimelineView : Grid
+    {
+        public AgendaTimelineView()
+        {
+            this.InitializeComponent();
+            CreateTimeRows();
+        }
+
+        private void CreateTimeRows()
+        {
+            for(int i = 0; i < 96;  i++)
+            {
+                RowDefinition rowDefinition = new RowDefinition();
+                rowDefinition.Height = new GridLength(1, GridUnitType.Star);
+
+                TimeGrid.RowDefinitions.Add(rowDefinition);
+            }
+
+        }
+    }
+}

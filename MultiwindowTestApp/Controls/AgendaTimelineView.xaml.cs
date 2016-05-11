1ff01c6f4eb2398d495705c01418868a1cc4f9b9
Raphael Zenhäusern <raphael@zenhausern.net>
2016-05-11 20:36:25 +0200

    Adjusted TimeGrid layout

--- MultiwindowTestApp/Controls/AgendaTimelineView.xaml.cs
+++ MultiwindowTestApp/Controls/AgendaTimelineView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MultiwindowTestApp.Models;
+using MultiwindowTestApp.Utilitys;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,6 +15,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -20,6 +23,14 @@ namespace MultiwindowTestApp.Controls
 {
     public sealed partial class AgendaTimelineView : Grid
     {
+        public List<AgendaDay> AgendaDays {
+            get { return AgendaDays; }
+            set {
+                AgendaDays = value;
+                FillContentGrid();
+            }
+        }
+
         public AgendaTimelineView()
         {
             this.InitializeComponent();
@@ -28,8 +39,14 @@ namespace MultiwindowTestApp.Controls
 
         private void CreateTimeRows()
         {
-            Brush borderBrush = new SolidColorBrush(Colors.Gray);
-            for(int i = 0; i < 96;  i++)
+            
+            Brush filledBorderBrush = (Brush)Resources["SystemControlForegroundBaseLowBrush"];
+            Brush dashedBorderBrush = (Brush)TimeGrid.Resources["HorizontalDashedBrush"];
+            Brush dottedBorderBrush = (Brush)TimeGrid.Resources["HorizontalDottedBrush"];
+            Brush timeStampBrush = (Brush)Resources["ButtonDisabledForegroundThemeBrush"];
+            Thickness timeStampMargin = new Thickness(0, 0, 8, 0);
+
+            for (int i = 0; i < 96;  i++)
             {
                 RowDefinition rowDefinition = new RowDefinition();
                 rowDefinition.Height = new GridLength(1, GridUnitType.Star);
@@ -37,13 +54,96 @@ namespace MultiwindowTestApp.Controls
                 TimeGrid.RowDefinitions.Add(rowDefinition);
 
                 Border separatorLine = new Border();
-                separatorLine.BorderThickness = new Thickness(0, 0, 0, 1);
-                separatorLine.BorderBrush = borderBrush;
 
                 TimeGrid.Children.Add(separatorLine);
                 Grid.SetRow(separatorLine, i);
-                Grid.SetColumnSpan(separatorLine, 2);
+                Grid.SetColumn(separatorLine, 1);
+
+                separatorLine.BorderThickness = new Thickness(0, 0, 0, 1);
+
+                if(i % 4 == 3)
+                {
+                    // We do this if inside here, so the if-else loop gets in here and not to the ultimate esle if i=95
+                    if(i != 95)
+                    {
+                        separatorLine.BorderBrush = filledBorderBrush;
+                        Grid.SetColumn(separatorLine, 0);
+                        Grid.SetColumnSpan(separatorLine, 2);
+                    }
+                } else if(i % 4 == 1)
+                {
+                    separatorLine.BorderBrush = dashedBorderBrush;
+                } else
+                {
+                    separatorLine.BorderBrush = dottedBorderBrush;
+                }
+
+                if(i % 4 == 0)
+                {
+                    TextBlock timeStamp = new TextBlock();
+                    timeStamp.Text = $"{((i +3) / 4)}";
+                    timeStamp.FontSize = 20;
+                    timeStamp.Foreground = timeStampBrush;
+                    timeStamp.HorizontalAlignment = HorizontalAlignment.Right;
+                    timeStamp.Margin = timeStampMargin;
+
+                    TimeGrid.Children.Add(timeStamp);
+                    Grid.SetRow(timeStamp, i);
+                }
             }
+        }
+
+        private void AdjustContentGrid()
+        {
+            int numOfDays = AgendaDays.Count;
+
+            if(ContentGrid.ColumnDefinitions.Count < numOfDays)
+            {
+                while(ContentGrid.ColumnDefinitions.Count < numOfDays)
+                {
+                    ColumnDefinition colDefinition = new ColumnDefinition();
+                    colDefinition.Width = new GridLength(1, GridUnitType.Star);
+                    ContentGrid.ColumnDefinitions.Add(colDefinition);
+                }
+            } else if(ContentGrid.ColumnDefinitions.Count > numOfDays)
+            {
+                while(ContentGrid.ColumnDefinitions.Count > numOfDays)
+                {
+                    ContentGrid.ColumnDefinitions.RemoveAt(ContentGrid.ColumnDefinitions.Count -1);
+                }
+            }
+        }
+
+        private void AdjustHeaderGrid()
+        {
+
+        }
+
+        private void FillContentGrid()
+        {
+            // breaking condition for the errorcase that we have no agendadays
+            if (AgendaDays.Count == 0)
+            {
+                return;
+            }
+
+            // If column count doesn't match the amount of days, adjust the grid
+            if (ContentGrid.ColumnDefinitions.Count != AgendaDays.Count)
+            {
+                AdjustContentGrid();
+            }
+
+            // calculate the amount of different header columns needed
+            AgendaDays.Count * AgendaDays.First().AppointmentsPerUser.Count();
+            if(AgendaDays.Count > 0)
+            {
+
+            }
+            if(ColumnDefinitions.Count != 0)
+            {
+
+            }
+
 
         }
     }
---
+++ MultiwindowTestApp/Models/AgendaDay.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiwindowTestApp.Models
+{
+    public class AgendaDay
+    {
+        public DateTime Date { get; set; }
+        public Dictionary<string, ObservableCollection<Appointment>> AppointmentsPerUser { get; set; }
+    }
+}
